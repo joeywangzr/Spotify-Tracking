cb74205795a3e949d4ed774651968843cd03a0a7
Joey Wang <joeywangzr@gmail.com>
2022-01-23 00:10:52 -0700

    commit

--- profileinfo.py
+++ profileinfo.py
@@ -1,3 +1,4 @@
 username = "joey_kangaroos"
 client_id = 'b4030afd8b4d4d199f5d643ca00534b0'
 client_secret = '05b2ac4ab3e041b28cdbe73ec160ffbb'
+cluster = 'mongodb+srv://jwang:20040122w@spotify.1tnq6.mongodb.net/spotify?retryWrites=true&w=majority'
